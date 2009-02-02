6b6261f7998f411956746776dc6700ea9be18bc9
Peter Jaros <peeja@dropio.com>
2009-02-02 16:39:36 -0500

    More sensible spec.

--- spec/for_each_row_spec.rb
+++ spec/for_each_row_spec.rb
@@ -5,14 +5,17 @@ require 'spec'
 require 'for_each_row'
 
 describe ForEachRow do
+  include ForEachRow
+  
   it "should call the block once with each row" do
-    block = lambda {}
-    block.should_receive(:call).once.with(1, 2, 3)
-    block.should_receive(:call).once.with(5, 7, 12)
+    should_receive(:called_with).once.with(1, 2, 3)
+    should_receive(:called_with).once.with(5, 7, 12)
     
-    for_each_row <<-TABLE, &block
-                      |1,       2,       3  |
-                      |5,       7,       12 |
+    for_each_row <<-TABLE do |number1, number2, sum|
+                             |1,       2,       3  |
+                             |5,       7,       12 |
                     TABLE
+      called_with(number1, number2, sum)
+    end
   end
 end
